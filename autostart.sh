41e792bab2caa71ec43fe5edb3d78d91b7b0614d
DomGlusk <64805993+TheKarion@users.noreply.github.com>
2022-10-07 16:32:39 -0500

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,23 +1,33 @@
 #!/bin/sh
 #feh --bg-scale /usr/share/endeavouros/backgrounds/endeavouros-wallpaper.png
+#nvidia-settings --load-config-only & disown
 nitrogen --restore & disown
-picom & disown # --experimental-backends --vsync should prevent screen tearing on most setups if needed
+picom --vsync --experimental-backends & disown
+#tint2 & disown
+#xcompmgr & disown
 
+#sudo system76-power daemon & disown
+#dunst & disown
 # Low battery notifier
 ~/.config/qtile/scripts/check_battery.sh & disown
 
 # Start welcome
 # eos-welcome & disown
 
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & disown # start polkit agent from LXDE/LXQT
+/usr/bin/lxpolkit & disown # start polkit agent from LXDE/LXQT
+/usr/lib/xfce4/notifyd/xfce4-notifyd & disown
+#redshift-gtk & disown
+#steam & disown
+#discord & disown
+#spotify  & disown
 
-redshift-gtk & disown
-steam & disown
-discord & disown
-spotify & disown
-spectacle -b & disown
-
-# lxsession --noautostart -s Qtile -e Qtile & disown
+lxsession -s=Qtile -a & disown
+#xfce4-session & disown
 
 blueman-applet & disown
 nm-applet & disown
+#mullvad-vpn & disown
+#cbatticon & disown
+#volumeicon & disown
+#prime-offload & disown
+#optimus-manager-qt & disown
